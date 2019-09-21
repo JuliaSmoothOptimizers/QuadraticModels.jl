aae9a2945ddffad7b3046248eca4ec06ee9a8d29
RenanOD <renan_oclides@hotmail.com>
2019-09-21 17:04:46 -0300

    add tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,21 @@
-using QuadraticModels
+using Test, QuadraticModels, SparseArrays, NLPModels, NLPModelsIpopt, LinearOperators
 
-true
+@testset "qpsreader" begin
+	c0        = 0.0
+	c         = [1.5; -2.0]
+	Q         = sparse([1; 2; 2], [1; 1; 2], [8.0; 2.0; 10.0])
+	A         = sparse([1; 2; 1; 2], [1; 1; 2; 2], [2.0; -1.0; 1.0; 2.0])
+	lcon      = [2.0; -Inf]
+	ucon      = [Inf; 6.0]
+	uvar      = [20; Inf]
+	lvar      = [0.0; 0.0]
+	objective = 4.3718750e+00
+
+	qp     = QuadraticModel(c, Q, opHermitian(Q), A, lcon, ucon, lvar, uvar, c0 = c0)
+	output = ipopt(qp, print_level = 0)
+
+	@test output.dual_feas                      < 1e-6
+	@test output.primal_feas                    < 1e-6
+	@test maximum(output.objective - objective) < 1e-8
+
+end # testset
