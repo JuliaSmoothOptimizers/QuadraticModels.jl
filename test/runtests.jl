a0d866de72e6e93155f83719fba48f13f9f785b7
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2020-03-27 18:51:05 -0300

    Modify QuadraticModels to NLPModels 0.12

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,13 @@
 using Printf, SparseArrays, Test
 
 # our packages
-using LinearOperators, NLPModels, QuadraticModels
+using LinearAlgebra, LinearOperators, NLPModels, QuadraticModels
 
+nlpmodels_path = joinpath(dirname(pathof(NLPModels)), "..", "test")
+nlpmodels_problems_path = joinpath(nlpmodels_path, "problems")
+
+# Definition of the quadratic problem in ADNLPModel
 include("simpleqp.jl")
+
+include(joinpath(nlpmodels_path, "consistency.jl"))
 include("test_consistency.jl")
--- test/simpleqp.jl
+++ test/simpleqp.jl
@@ -2,7 +2,7 @@
 function simpleqp_autodiff()
 
   x0   = [-1.; 1.]
-  f(x) = 4*x[1]^2 + 5*x[2]^2 + x[1]*x[2] + 1.5*x[1] - 2*x[2]
+  f(x) = 4*x[1]^2 + 5*x[2]^2 + x[1]*x[2] + 1.5*x[1] - 2*x[2] + 1.0
   c(x) = [2*x[1] + x[2]; -x[1] + 2*x[2]]
   lcon = [2.0; -Inf]
   ucon = [Inf; 6.0]
@@ -10,5 +10,18 @@ function simpleqp_autodiff()
   lvar = [0.0; 0.0]
 
   return ADNLPModel(f, x0, c=c, lcon=lcon, ucon=ucon, lvar=lvar, uvar=uvar)
+end
 
+function simpleqp_QP()
+  c     = [1.5; -2.0]
+  H     = [8.0 0.0; 1.0 10.0]
+  A     = [2.0 1.0; -1.0 2.0]
+  c0    = 1.0
+  lcon  = [2.0; -Inf]
+  ucon  = [Inf; 6.0]
+  uvar  = [20; Inf]
+  lvar  = [0.0; 0.0]
+  x0    = [-1.0; 1.0]
+
+  return QuadraticModel(c, H, A=A, lcon=lcon, ucon=ucon, lvar=lvar, uvar=uvar, c0=c0, x0=x0)
 end
