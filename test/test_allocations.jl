690b9381a64a1dccd7e18be73bfa9914238abd7b
Alexis Montoison <35051714+amontoison@users.noreply.github.com>
2025-05-18 11:45:26 -0500

    Fix the unit tests (#150)
    * Fix the unit tests
    
    * Test the linear API
    
    * Use pre-release instead of nightly in CI

--- test/test_allocations.jl
+++ test/test_allocations.jl
@@ -11,28 +11,32 @@ end
   @testset "allocs QPSData" begin
     for problem in qp_problems_Matrix
       nlp_qps = eval(Symbol(problem * "_QPSData"))()
-      test_only_zeros(test_allocs_nlpmodels(nlp_qps))
+      allocs = test_allocs_nlpmodels(nlp_qps; linear_api=true, exclude=[jac_op])
+      test_only_zeros(allocs)
     end
   end
 
   @testset "allocs QP_dense" begin
     for problem in qp_problems_Matrix
       nlp_qm_dense = eval(Symbol(problem * "_QP_dense"))()
-      test_only_zeros(test_allocs_nlpmodels(nlp_qm_dense))
+      allocs = test_allocs_nlpmodels(nlp_qm_dense; linear_api=true, exclude=[jac_op])
+      test_only_zeros(allocs)
     end
   end
 
   @testset "allocs COO QPSData" begin
     for problem in qp_problems_COO
       nlp_qps = eval(Symbol(problem * "_QPSData"))()
-      test_only_zeros(test_allocs_nlpmodels(nlp_qps))
+      allocs = test_allocs_nlpmodels(nlp_qps; linear_api=true, exclude=[jac_op])
+      test_only_zeros(allocs)
     end
   end
 
   @testset "allocs COO QP" begin
     for problem in qp_problems_COO
       nlp_qm_dense = eval(Symbol(problem * "_QP"))()
-      test_only_zeros(test_allocs_nlpmodels(nlp_qm_dense))
+      allocs = test_allocs_nlpmodels(nlp_qm_dense; linear_api=true, exclude=[jac_op])
+      test_only_zeros(allocs)
     end
   end
 
@@ -41,7 +45,8 @@ end
       nlp = eval(Symbol(problem))()
       x = nlp.meta.x0
       nlp_qm = QuadraticModel(nlp, x)
-      test_only_zeros(test_allocs_nlpmodels(nlp_qm))
+      allocs = test_allocs_nlpmodels(nlp_qm; linear_api=true, exclude=[jac_op])
+      test_only_zeros(allocs)
     end
   end
 end
